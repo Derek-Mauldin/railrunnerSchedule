f096ec33c5e6d0d4286e58472cb88ef2feb51a2e
Anthony Williams <mylifeassurance@aol.com>
2016-05-14 14:41:16 -0600

    Added images to media

---
+++ app/header.ts
@@ -0,0 +1,16 @@
+// header.component.ts
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'header',
+	template: `
+    <div class="header">
+      Rail runner routes coming soon!
+      <main>
+        <!-- stuff -->
+      </main>
+    </div>
+  `
+})
+export class HeaderComponent { }
+
