6e0be0a1dcf3eb31c999b83ee0c861f572e22c54
Derek Mauildin <derek.e.mauldin@gmail.com>
2016-05-13 23:14:12 -0600

    all kinds of frustrating stuff

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var knights_component_1 = require('./knights.component');
+platform_browser_dynamic_1.bootstrap(knights_component_1.KnightsComponent);
+//# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { KnightsComponent } from './knights.component';
+
+bootstrap(KnightsComponent);
