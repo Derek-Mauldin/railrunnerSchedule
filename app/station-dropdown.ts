54df7d4ecfb011b0a461b87c63b9892cf637c91c
Derek Mauildin <derek.e.mauldin@gmail.com>
2016-05-14 19:14:27 -0600

    finished station-dropdown component

--- app/station-dropdown.ts
+++ app/station-dropdown.ts
@@ -1,36 +1,29 @@
-import { Component } from '@angular/core';
-import { EventEmitter } from '@angular/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
-import {SELECT_DIRECTIVES} from 'ng2-select/ng2-select';
-import { BUTTON_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { Component, EventEmitter } from '@angular/core';
+import { NgForm }    from '@angular/common';
 
-
-@Component ({
+@Component({
 	selector: 'station-dropdown',
 	templateUrl: 'app/template/station-dropdown.html',
-	inputs: ['items'],
-	outputs: ['stationChosen'],
-	directives: [SELECT_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES, BUTTON_DIRECTIVES],
+	outputs: ['stationSelected']
 })
 
 export class StationDropdown {
 
-	/**
-	 * @output stationChosen outputs the chosen station
-	 */
-	stationChosen: EventEmitter<string>;
+	stationSelected : EventEmitter<string>;
 
-	private items: Array<string> = [ 'Belen', 'Los Lunas', 'Isleta Pueblo', 'Bernalillo County',
+	stations: Array<string> = [ 'Belen', 'Los Lunas', 'Isleta Pueblo', 'Bernalillo County',
 		'Downtown ABQ', 'Montano', 'Los Ranchos/Journal Center', 'Sandia Pueblo', 'Downtown Bernalillo',
 		'Sandoval/US 550', 'Kewa', 'SF County/NM 599', 'South Capitol', 'Santa Fe Depot' ];
 
+
+
 	constructor() {
-		this.stationChosen = new EventEmitter();
+		this.stationSelected = new EventEmitter();
 	}
 
-	private selected(value:any) {
-		console.log('Selected value is: ', value);
+	selected(station) {
+		console.log(station.value)
+		this.stationSelected.emit(station);
 	}
-
 
 }
